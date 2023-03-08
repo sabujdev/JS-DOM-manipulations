c2a9e988221437c21c0fa10ac7a04dc2f696bf0a
sabujmahonto <sabujmahonto@gmail.com>
2023-03-08 22:46:17 +0600

    How to Css style all element or attributes Done

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 //  DOM manipulations
 // 1. Selecting Element
     // getElementsById -- fast / rarely used
-    const title = document.getElementById('main-heading');
-    console.log(title);
+    // const title = document.getElementById('main-heading');
+    // console.log(title);
     //getElementsByClassName
     // const country = document.getElementsByClassName("country");
     // console.log(country);
@@ -12,12 +12,12 @@
     console.log(tag);
     //querySelector
     // * Used most of the cases
-    const country = document.querySelector(".country");
-    console.log(country);
+    // const country = document.querySelector(".country");
+    // console.log(country);
 
         //querySelectorAll
-    const countryAll = document.querySelectorAll(".country");
-    console.log(countryAll);
+    // const countryAll = document.querySelectorAll(".country");
+    // console.log(countryAll);
 
 
 
@@ -25,6 +25,24 @@
 
 
 // 2. Styling Element
+const container = document.querySelector(".container")
+    container.style.margin =" 80px auto"
+const title = document.querySelector("#main-heading");
+title.style.background = "blue";
+title.style.color = 'whiteSmoke';
+title.style.fontSize ="3rem"                // all style are made inline style
+title.textContent= "All Country List"
+
+
+const countries = document.querySelectorAll(".country");
+
+for(let i = 0; i < countries.length; i++){
+    countries[i].style.color = "orangeRed";
+    countries[i].style.fontSize = "2rem";
+    
+}
+console.log(countries);
+
 // 3. Creating Element
 // 4. Node traversals
 // 5. Event Handlers
